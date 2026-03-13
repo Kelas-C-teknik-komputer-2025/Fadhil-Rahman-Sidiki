c0cedec71b2c766a9076765137070eb04b61b2d9
fadhil2314 <fadhilsidiki2314@gmail.com>
2026-03-13 20:51:00 +0800

    Refactor MainProgram to use MataKuliah class

--- MainProgram.java
+++ MainProgram.java
@@ -7,65 +7,9 @@
  *
  * @author Fadhil2314
  */
-
 import java.util.Scanner;
 
 public class MainProgram {
-    private int sks;
-    private String nilaiHuruf;
-    private double nilaiBobot;
-    
-    public static double konversiNilai(String nilaiHuruf) {
-        switch (nilaiHuruf) {
-            case "A" -> {
-                return 4.0;
-            }
-            case "A-" -> {
-                return 3.7;
-            }
-            case "B+" -> {
-                return 3.3;
-            }
-            case "B" -> {
-                return 3.0;
-            }
-            case "B-" -> {
-                return 2.7;
-            }
-            case "C+" -> {
-                return 2.3;
-            }
-            case "C" -> {
-                return 2.0;
-            }
-            case "D" -> {
-                return 1.0;
-            }
-            case "E" -> {
-                return 0.0;
-            }
-            default -> {
-                return 0.0;
-            }
-        }
-    }
-    
-    public MainProgram(int sks, String nilaiHuruf) {
-        this.sks = sks;
-        this.nilaiHuruf = nilaiHuruf;
-        this.nilaiBobot = konversiNilai(nilaiHuruf);
-    }
-    
-    public double ambilNilaiMutu() {
-        return nilaiBobot * sks;
-    }
-    
-    public int ambilSKS() {
-        return sks;
-    }
-    public double ambilNilaiBobot() {
-        return nilaiBobot;
-    }
     
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
@@ -76,7 +20,7 @@ public class MainProgram {
         int totalMK = keyboard.nextInt();
         keyboard.nextLine();
         
-        MainProgram[] listMK = new MainProgram[totalMK];
+        MataKuliah[] listMK = new MataKuliah[totalMK];
 
         for (int i = 0; i < totalMK; i++) {
             System.out.println("\nMata Kuliah ke-" + i);
@@ -89,12 +33,12 @@ public class MainProgram {
             System.out.print("Nilai Huruf (A, A-, B+, B, B-, C+, C, D, E): ");
             String nilaiHuruf = keyboard.nextLine().trim().toUpperCase();
             
-            listMK[i] = new MainProgram(sks, nilaiHuruf);
+            listMK[i] = new MataKuliah(sks, nilaiHuruf);
         }
         
-        for (MainProgram mk : listMK) {
-            totalNilaiMutu += mk.ambilNilaiMutu();
-            totalSKS += mk.ambilSKS();
+        for (MataKuliah mk : listMK) {
+            totalNilaiMutu += mk.getNilaiBobot();
+            totalSKS += mk.getSKS();
         }
         
         double totalIPS = totalNilaiMutu / totalSKS;
